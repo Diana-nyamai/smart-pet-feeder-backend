0435763d0caaa83b0aa9c55b4a90bd88985fb044
Diana-nyamai <dnyamai.dn@gmail.com>
2026-04-30 04:48:23 +0300

    fix: remove dev/ttyusb0

--- src/services/feederService.js
+++ src/services/feederService.js
@@ -1,25 +1,7 @@
-const { SerialPort } = require('serialport');
-
-// const port = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
-
 exports.feedCat = () => {
-    try {
-        port = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
-        port.write('FEED\n', (err) => {
-            if (err) {
-                console.error('Error writing to serial port:', err);
-                throw new Error('Failed to send feed command to the cat feeder');
-            }
-            console.log('Feed command sent to the cat feeder');
-        });
-    } catch (error) {
-        console.error('Error in feedCat service:', error);
-        throw error;
-    }
-
     return {
         status: 'success',
         message: 'The cat has been fed successfully!',
         time: new Date().toISOString()
-    }
+    };
 }
